2a96681d461d2d22e2f6fc94afc5515675faa0ce
Rocka84 <f.dillmeier@gmail.com>
2021-11-19 09:03:06 +0100

    status led communication: added kana
    there are 5 leds! :doh:
    5 bits means more signals :-)

--- shorty_lights.sh
+++ shorty_lights.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 
+# Set backlight and button highlights on a shorty keyboard.
+#
+# Details:
+# Sends signals to shorty keyboard via the status LEDs.
+# USB Keyboards receive the state their LEDs should be in as one byte.
+# The 3 upper most bits are constant, we use on bit as a latching bit
+# and are left with 4 bits. This allows us to transmit 15 distinct signals
+# to the shorty firmware.
+#
+# data ------------\
+# latch ---------\ |
+#                |/|\_
+#              0b00000
+#                |||||
+# LED_KANA ------/||||
+# LED_COMPOSE ----/|||
+# LED_SCROLLLOCK --/||
+# LED_CAPSLOCK -----/|
+# LED_NUMLOCK -------/
 
-findDeviceNumber(){
+device_path=
+
+function help() {
+    cat <<-END
+Usage:
+$0 [ clear | light [0|1] | highlight [1-6] [0|1] ] [...]
+    clear      turn off backlight and all highlights
+    light      turn backlight on (1) or off (0)
+    highlight  turn highlight of button [index] on (1) or off (0)
+
+Multiple commands can be chained:
+    $0 clear highlight 1 1 highlight 6 1
+END
+    exit ${1:-0}
+}
+
+findDeviceNumber(){ # stackoverflow to the rescue!
     v=${1%:*}; p=${1#*:}  # split vid:pid into 2 vars
     v=${v#${v%%[!0]*}}; p=${p#${p%%[!0]*}}  # strip leading zeros
     grep -il "^PRODUCT=$v/$p" /sys/bus/usb/devices/*:*/uevent |
@@ -10,71 +45,93 @@ findDeviceNumber(){
     grep input | sed 's/.*input\/input\([0-9]\+\)\/event.*$/\1/'
 }
 
-# data -------------\
-# latch ----------\ |
-#                 |/|\
-#               0b0000
-#                 ||||
-# LED_COMPOSE ----/|||
-# LED_SCROLLLOCK --/||
-# LED_CAPSLOCK -----/|
-# LED_NUMLOCK -------/
-
-
-device_number="$(findDeviceNumber 03eb:2042)"
-if [ -z "$device_number" ]; then
-    echo "device not found"
-    exit 1
-fi
-
-device_path="/sys/class/leds/input$device_number::LED/brightness"
+function findDevicePath() {
+    device_number="$(findDeviceNumber 03eb:2042)"
+    if [ -n "$device_number" ]; then
+        device_path="/sys/class/leds/input$device_number::LED/brightness"
+    fi
+}
 
 function setLed() {
     echo "$2" | sudo tee ${device_path/LED/$1} > /dev/null
 }
 
 function latch() {
-    setLed compose "1"
-    sleep .15
-    setLed compose "0"
-    sleep .15
+    setLed kana "1"
+    sleep .1
+    setLed kana "0"
+    sleep .1
 }
 
 function sendBits() {
+    [ -z "$device_path" ] && findDevicePath
+    if [ -z "$device_path" ]; then
+        echo "device not found"
+        exit 1
+    fi
+
+    # unlatch
+    setLed kana "0"
     setLed numlock "$1"
     setLed capslock "$2"
     setLed scrolllock "$3"
+    setLed compose "$4"
     latch
 }
 
-# unlatch
-setLed compose "0"
+
+
+if [ "$*" == "" ]; then
+    echo "No command given"
+    help 1
+fi
 
 while [ -n "$1" ]; do
     case "$1" in
+        "-h")
+            help
+            ;;
         "-d")
-            device_number="$2"
-            device_path="/sys/class/leds/input$device_number::LED/brightness"
+            device_path="/sys/class/leds/input$2::LED/brightness"
             shift
             ;;
-        "toggle_light")
-            # 0b000
-            sendBits 0 0 0
+        "light")
+            # 0bX000
+            state=0
+            [ "$2" == "1" ] && state=1
+            sendBits 0 0 0 "$state"
+            shift
+            ;;
+        "light_on")
+            # 0b1000 / 8
+            sendBits 0 0 0 1
+            ;;
+        "light_off")
+            # 0b0000
+            sendBits 0 0 0 0
             ;;
         "highlight")
             # 0b001 - 0b110 / 1 - 6
-            [ -z "$2" ] && exit 2
-            [ $2 -gt 6 ] && exit 2
-            sendBits "$(($2 & 1))" "$(($(($2 >> 1)) & 1))" "$(($(($2 >> 2)) & 1))"
-            shift
+            [ -z "$2" ] && help 2
+            [ -z "$3" ] && help 2
+            [ $2 -gt 6 ] && help 2
+            state=0
+            [ "$3" == "1" ] && state=1
+
+            sendBits "$(($2 & 1))" "$(($(($2 >> 1)) & 1))" "$(($(($2 >> 2)) & 1))" "$state"
+            shift 2
             ;;
         "clear")
-            # 0b111 / 7
-            sendBits 1 1 1
+            # 0b0111 / 7
+            sendBits 1 1 1 0
+            ;;
+        "effect")
+            # 0b1111 / 15
+            sendBits 1 1 1 1
             ;;
         *)
-            echo "unknown command"
-            exit 1
+            echo "unknown command '$1'"
+            help 1
     esac
     shift
 done
--- src/main.cpp
+++ src/main.cpp
@@ -35,15 +35,25 @@ Adafruit_NeoPixel pixels(BUTTON_COUNT, PIN_NEOPIXELS, NEO_GRB + NEO_KHZ800);
 
 bool backlight = false;
 uint32_t color_default = pixels.Color(3, 3, 3);
-uint32_t color_lit = pixels.Color(0, 0, 30);
 uint32_t color_off = pixels.Color(0, 0, 0);
+
+int button_colors[] = { 0, 0, 0, 0, 0, 0 };
+uint32_t colors[] = {
+    pixels.Color(0, 0, 30),
+    pixels.Color(0, 20, 20),
+    pixels.Color(0, 30, 0),
+    pixels.Color(20, 20, 0),
+    pixels.Color(30, 0, 0),
+    pixels.Color(20, 0, 20),
+    pixels.Color(12, 12, 12)
+};
 
 bool led_latch_handled = false;
 
 void handleLedStatus() {
     uint8_t led_status = Keyboard.readLedStatus();
 
-    int latch = led_status >> 3;
+    int latch = led_status >> 4; // bit 5
     if (latch == 1 && led_latch_handled) {
         return;
     }
@@ -53,17 +63,34 @@ void handleLedStatus() {
     }
 
     led_latch_handled = true;
-    int data = led_status & 7;
+    bool target_state = ((led_status >> 3 & 1) == 1); // bit 4
+    int data = led_status & 7; // bits 1-3
 
-    if (data == 0) { // b000
-        backlight = !backlight;
-    } else if (data >= 1 && data <= 6) { // b001 - b110
-        buttons_lit[data - 1] = !buttons_lit[data - 1];
-    } else if (data == 7) { // b111
-        for (int i = 0; i < BUTTON_COUNT; i++) {
-            buttons_lit[i] = false;
+    if (data == 0) { // 0b0000 / 0b1000
+        backlight = target_state;
+
+    } else if (data >= 1 && data <= 6) { // 0bX001 - 0bX110
+        if (target_state && !buttons_lit[data - 1]) { // turn on
+            buttons_lit[data - 1] = true;
+        } else if (target_state) { // already on, next color
+            button_colors[data - 1]++;
+            if (button_colors[data - 1] > 6) button_colors[data - 1] = 0;
+
+        } else if (buttons_lit[data - 1]) { // turn off
+            buttons_lit[data - 1] = false;
+        } else { // already off, reset color
+            button_colors[data - 1] = 0;
         }
-        backlight = false;
+
+    } else if (data == 7) { // 0bX111
+        if (!target_state) { // 0b0111
+            for (int i = 0; i < BUTTON_COUNT; i++) {
+                buttons_lit[i] = false;
+                button_colors[i] = 0;
+            }
+        } else { // 0b1111
+            //@ToDo: add simple effect
+        }
     }
 }
 
@@ -108,7 +135,7 @@ void loop() {
             pixels.setPixelColor(i, pixels.Color(0, 20, 0));
 
         } else if (buttons_lit[i]) {
-            pixels.setPixelColor(i, color_lit);
+            pixels.setPixelColor(i, colors[button_colors[i]]);
 
         } else if(backlight) {
             pixels.setPixelColor(i, color_default);
@@ -124,6 +151,6 @@ void loop() {
 
     pixels.show();
 
-    delay(100);
+    delay(50);
 }
 
